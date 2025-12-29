f4e22f6ae4f772530cec3af0ed6c965db4ab8c5b
shatrio <117924027+wiluyo12@users.noreply.github.com>
2025-12-30 01:49:52 +0700

    fix login issue

--- app/login/page.js
+++ app/login/page.js
@@ -53,12 +53,24 @@ export default function Login() {
         }
     };
 
+    const getURL = () => {
+        let url =
+            process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+            process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+            'http://localhost:3000/';
+        // Make sure to include `https://` when not localhost.
+        url = url.includes('http') ? url : `https://${url}`;
+        // Make sure to include a trailing `/`.
+        url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+        return url;
+    };
+
     const handleGoogleLogin = async () => {
         setLoading(true);
         const { error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: `${window.location.origin}${redirectTo}`,
+                redirectTo: `${getURL()}${redirectTo.startsWith('/') ? redirectTo.slice(1) : redirectTo}`,
             },
         });
         if (error) {
